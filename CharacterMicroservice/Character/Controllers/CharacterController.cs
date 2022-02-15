00b167e17db3918ca91221664d174d668d3cf837
gvang146 <gvang146@gmail.com>
2022-02-15 14:40:34 -0600

    Setup Application API and database

---
+++ CharacterMicroservice/Character/Controllers/CharacterController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Character.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CharacterController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<CharacterController> _logger;
+
+        public CharacterController(ILogger<CharacterController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public Character Get()
+        {
+            Character chara = new Character();
+            return chara;
+        }
+    }
+}
