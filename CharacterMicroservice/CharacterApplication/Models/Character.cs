630e89b00624c0eb5b6256792100c8febe1a0349
gvang146 <gvang146@gmail.com>
2022-02-17 11:36:22 -0600

    return data as a string

---
+++ CharacterMicroservice/CharacterApplication/Models/Character.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterApplication.Models
+{
+    public class Character
+    {
+        private int _id;
+        private string _name;
+        private string _power;
+        private string _description;
+        private string _type;
+        private int _rank;
+
+        public Character()
+        {
+        }
+        public Character(int id)
+        {
+            ID = id;
+        }
+        public Character(string name)
+        {
+            Name = name;
+        }
+        public Character(int id, string name, string power, string description, string type, int rank)
+        {
+            ID = id;
+            Name = name;
+            Power = power;
+            Description = description;
+            Type = type;
+            Rank = rank;
+        }
+        public int ID
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+        public string Power
+        {
+            get { return _power; }
+            set { _power = value; }
+        }
+        public string Description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+        public string Type
+        {
+            get { return _type; }
+            set { _type = value; }
+        }
+        public int Rank
+        {
+            get { return _rank; }
+            set { _rank = value; }
+        }
+    }
+}
--- CharacterMicroservice/CharacterApplication/Program.cs
+++ CharacterMicroservice/CharacterApplication/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CharacterApplication.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
@@ -9,24 +10,57 @@ namespace CharacterApplication
 {
     class Program
     {
-        static void Main(string[] args)
+        
+        private static readonly HttpClient client = new HttpClient();
+        public static async Task Main(string[] args)
         {
-            HttpClient client = new HttpClient();
+            //Creating a character to post it to the database
+            Character character = new Character();
+            character.Name = "Polyphemus";
+            character.Power = "Giant";
+            character.Description = "Polyphemus is a giant cyclop and is also the son of Poseidon in Greek Mythologies";
+            character.Type = "Monster";
+            character.Rank = 56;
+
             Console.WriteLine("Calling the Character API... ");
             Console.WriteLine();
             var response = client.GetAsync("http://localhost:5000/api/Character");
+            var responseSingle = client.GetAsync("http://localhost:5000/api/Character/620d288c12e8e4de6f0ebf51");
             response.Wait();
-            if (response.IsCompleted)
+            responseSingle.Wait();
+            //running and returning individual characters
+            try
             {
-                var result = response.Result;
-                if (result.IsSuccessStatusCode)
+                if (response.IsCompleted)
                 {
-                    var contents = result.Content.ReadAsStringAsync();
-                    contents.Wait();
-                    Console.WriteLine("Data from web API: " + contents.Result);
-                    Console.ReadKey();
+                    var result = response.Result;
+                    if (result.IsSuccessStatusCode)
+                    {
+                        var allCharacters = result.Content.ReadAsStringAsync();
+                        allCharacters.Wait();
+                        Console.WriteLine("Printing All Character in a Single String...");
+                        Console.WriteLine(allCharacters.Result);
+                        Console.WriteLine();
+                    }
                 }
+                Console.WriteLine();
+                if (responseSingle.IsCompleted)
+                {
+                    var result2 = response.Result;
+                    if (result2.IsSuccessStatusCode)
+                    {
+                        var singleChar = result2.Content.ReadAsStringAsync();
+                        singleChar.Wait();
+                    }
+                }
+                Console.ReadKey();
             }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            
         }
     }
 }
