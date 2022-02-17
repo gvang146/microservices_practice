92f042ee0318f3ff3e1d71db0ec5882c491f53ec
gvang146 <gvang146@gmail.com>
2022-02-17 10:20:44 -0600

    Created and connect api to console

---
+++ CharacterMicroservice/CharacterApplication/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharacterApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HttpClient client = new HttpClient();
+            Console.WriteLine("Calling the Character API... ");
+            Console.WriteLine();
+            var response = client.GetAsync("http://localhost:5000/api/Character");
+            response.Wait();
+            if (response.IsCompleted)
+            {
+                var result = response.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var contents = result.Content.ReadAsStringAsync();
+                    contents.Wait();
+                    Console.WriteLine("Data from web API: " + contents.Result);
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
